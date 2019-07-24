c5749aa201f5b41c743639dbc78dc393f57d5275
claytonwyman <claytonwyman@gmail.com>
2019-07-24 14:00:19 -0600

    Removed unnnecessary bloat from tasks

--- client/src/components/Tasks.vue
+++ client/src/components/Tasks.vue
@@ -1,22 +1,13 @@
 <template>
     <div class="tasks">
-        <h1>{{list.title}}</h1>
+        
     </div>
 </template>
 
 <script>
 export default {
     name: 'tasks',
-    props: ['listId'],
-    computed: {
-      list() {
-        return this.$store.state.activeList
-      }
-    },
-    mounted(){
-        this.$store.dispatch("getActiveList", this.listId)
-    }
-}
+    props: ['listId']
 </script>
 
 <style>
